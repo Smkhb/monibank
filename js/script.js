caa32a24ac5ab450d00c40a77ef8bdfe964afbde
SAMUEL BERNARDO <108311581+Smkhb@users.noreply.github.com>
2023-01-30 14:13:49 -0300

    Aula: localStorage

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 import ehUmCPF from "./valida-cpf.js";
 import ehMaiorDeIdade from "./valida-idade.js";
-
 const camposDoFormulario = document.querySelectorAll('[required]');
+const formulario = document.querySelector('[data-formulario]');
 
+formulario.addEventListener('submit',e=>{
+    e.preventDefault();
+
+    const listaRspostas ={
+        'nome':e.target.elements['nome'].value,
+        'email':e.target.elements['email'].value,
+        'rg':e.target.elements['rg'].value,
+        'cpf':e.target.elements['cpf'].value,
+        'aniversario':e.target.elements['aniversario'].value,
+    }
+    localStorage.setItem('cadastro',JSON.stringify(listaRspostas));
+
+    window.location.href='./abrir-conta-form-2.html'
+})
 camposDoFormulario.forEach(campo=>{
     campo.addEventListener('blur',()=>verificaCampo(campo))
     campo.addEventListener("invalid", evento => evento.preventDefault())
