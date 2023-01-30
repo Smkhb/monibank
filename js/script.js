3fd6e9b060371ac826237f3b88cba950ad4926de
SAMUEL BERNARDO <108311581+Smkhb@users.noreply.github.com>
2023-01-30 11:57:56 -0300

    Aula: Validity State

--- js/script.js
+++ js/script.js
@@ -3,7 +3,12 @@ import ehMaiorDeIdade from "./valida-idade.js";
 
 const camposDoFormulario = document.querySelectorAll('[required]');
 
-camposDoFormulario.forEach(campo=>campo.addEventListener('blur',()=>verificaCampo(campo)))
+camposDoFormulario.forEach(campo=>{
+    campo.addEventListener('blur',()=>verificaCampo(campo))
+    campo.addEventListener("invalid", evento => evento.preventDefault())
+})
+
+
 
 function verificaCampo(campo){
     if(campo.name=="cpf"&& campo.value.length>=11){
@@ -12,4 +17,5 @@ function verificaCampo(campo){
     if(campo.name=="aniversario"&& campo.value !=''){
         ehMaiorDeIdade(campo)
     }
+    console.log(campo.validity)
 }
