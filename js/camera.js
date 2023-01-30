3532e3333722eb88cef0a541f9f8d4f6a28788f4
SAMUEL BERNARDO <108311581+Smkhb@users.noreply.github.com>
2023-01-30 14:41:15 -0300

    Aula: Salvar foto

--- js/camera.js
+++ js/camera.js
@@ -4,7 +4,9 @@ const video = document.querySelector('[data-video]');
 const botaoTirarFoto = document.querySelector('[data-tirar-foto]');
 const canvas = document.querySelector('[data-video-canvas]');
 const mensagem = document.querySelector('[data-mensagem]');
-const imagemURL = '';
+let imagemURL = '';
+const botaoEnviarFoto = document.querySelector('[data-enviar]');
+
 botaoIniciarCamera.addEventListener('click',async function(){
     const iniciarVideo = await navigator.mediaDevices.getUserMedia({video:true,audio:false});
 
@@ -19,4 +21,16 @@ botaoTirarFoto.addEventListener('click',function(){
     imagemURL = canvas.toDataURL('image/jpeg');
     campoCamera.style.display = 'none';
     mensagem.style.display = 'block';
+})
+
+botaoEnviarFoto.addEventListener('click',()=>{
+    const receberDadosExistentes = localStorage.getItem('cadastro');
+    const converteRetorno = JSON.parse(receberDadosExistentes);
+
+    converteRetorno.imagem = imagemURL;
+
+    localStorage.setItem('cadastro',JSON.stringify(converteRetorno));
+
+    window.location.href = './abrir-conta-form-3.html'
+
 })
